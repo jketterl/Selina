2726f620d2ef36f1e9f828f05bdbb743325cb162
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-01 21:30:56 +0100

    adding more fields

--- src/de/chipxonio/adtech/selrunner/hosts/Host.java
+++ src/de/chipxonio/adtech/selrunner/hosts/Host.java
@@ -1,8 +1,37 @@
 package de.chipxonio.adtech.selrunner.hosts;
 
 public class Host {
+	private String name;
+	private String hostName;
+	private int port;
+	private Object[] browsers;
+	
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String toString()
 	{
-		return "I am a host";
+		return this.getName();
 	}
 }
