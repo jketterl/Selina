1d4a697f4fd40b8f9cf2fefbbe71ff2c5f07ff51
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-02 09:06:40 +0100

    close dialog on escape key

--- src/de/chipxonio/adtech/selrunner/hosts/HostEditor.java
+++ src/de/chipxonio/adtech/selrunner/hosts/HostEditor.java
@@ -6,12 +6,18 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.WindowConstants;
 
 public class HostEditor extends JDialog {
 
@@ -51,10 +57,17 @@ public class HostEditor extends JDialog {
 	 */
 	private void initialize() {
 		this.setSize(new Dimension(445, 145));
+		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		this.setModal(true);
 		this.setTitle("Selenium Host bearbeiten");
 		this.setContentPane(getJContentPane());
 		this.getRootPane().setDefaultButton(this.getOkButton());
+		this.getRootPane().registerKeyboardAction(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
 	}
 
 	/**
@@ -208,6 +221,11 @@ public class HostEditor extends JDialog {
 		if (cancelButon == null) {
 			cancelButon = new JButton();
 			cancelButon.setText("Abbrechen");
+			cancelButon.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					dispose();
+				}
+			});
 		}
 		return cancelButon;
 	}
