10fdb488b5f82e896450feb7387789b969d87698
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-02 14:05:42 +0100

    not throwing it out (yet)

--- src/de/chipxonio/adtech/JSelTester.java
+++ src/de/chipxonio/adtech/JSelTester.java
@@ -1,15 +1,5 @@
 package de.chipxonio.adtech;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.SeleneseCommandExecutor;
-import org.openqa.selenium.remote.CommandExecutor;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import com.thoughtworks.selenium.DefaultSelenium;
-import com.thoughtworks.selenium.Selenium;
 
 public class JSelTester {
 
@@ -17,6 +7,7 @@ public class JSelTester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/*
 		URL host;
 		RemoteWebDriver driver;
 		try {
@@ -29,15 +20,18 @@ public class JSelTester {
 			DesiredCapabilities dc = new DesiredCapabilities();
 			dc.setBrowserName("safari");
 			driver = new RemoteWebDriver(exec, dc);
+			*/
 			/*
 			host = new URL("http://192.168.60.170:4444/wd/hub");
 			driver = new RemoteWebDriver(host, DesiredCapabilities.internetExplorer());
 			*/
+			/*
 			TestingThread t2 = new TestingThread(driver);
 			t2.start();
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
+		*/
 	}
 
 }
