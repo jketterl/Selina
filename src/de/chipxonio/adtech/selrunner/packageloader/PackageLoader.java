850a7d70de40f234ca8bef3a07884aad69601509
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-03 15:06:58 +0100

    made test package external

---
+++ src/de/chipxonio/adtech/selrunner/packageloader/PackageLoader.java
@@ -0,0 +1,7 @@
+package de.chipxonio.adtech.selrunner.packageloader;
+
+public class PackageLoader extends ClassLoader {
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		return null;
+	}
+}
