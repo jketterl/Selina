97bd4c816b34ed316072c5dbaa1a9d5a9aefdca4
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-06 00:32:55 +0100

    first steps rewriting the hostquehe

--- src/de/chipxonio/adtech/selrunner/engine/HostQueue.java
+++ src/de/chipxonio/adtech/selrunner/engine/HostQueue.java
@@ -1,17 +1,73 @@
 package de.chipxonio.adtech.selrunner.engine;
 
+import java.util.AbstractQueue;
 import java.util.Iterator;
-import java.util.Vector;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
 
 public class HostQueue extends Thread {
-	private Vector<SelRunnerTask> tasks = new Vector<SelRunnerTask>();
+	private boolean toBeStopped = false;
+	private Queue<SelRunnerTask> tasks = new AbstractQueue<SelRunnerTask>() {
+		private LinkedList<SelRunnerTask> list = new LinkedList<SelRunnerTask>();
+		
+		@Override
+		public boolean offer(SelRunnerTask arg0) {
+			if (arg0 == null) return false;
+			return this.list.add(arg0);
+		}
+
+		@Override
+		public SelRunnerTask peek() {
+			try {
+				return this.list.getFirst();
+			} catch (NoSuchElementException e) {
+				return null;
+			}
+		}
+
+		@Override
+		public SelRunnerTask poll() {
+			try {
+				return this.list.removeFirst();
+			} catch (NoSuchElementException e) {
+				return null;
+			}
+		}
+
+		@Override
+		public Iterator<SelRunnerTask> iterator() {
+			return this.list.iterator();
+		}
+
+		@Override
+		public int size() {
+			return this.list.size();
+		}
+	};
 	
 	public void add(SelRunnerTask task) {
 		this.tasks.add(task);
 	}
 	
 	public void run() {
-		Iterator<SelRunnerTask> i = this.tasks.iterator();
-		while (i.hasNext()) i.next().run();
+		while(!this.toBeStopped) {
+			SelRunnerTask task = this.tasks.poll();
+			if (task == null) {
+				try {
+					Thread.sleep(60000);
+					this.terminate();
+				} catch (InterruptedException e) {
+				}
+			} else {
+				task.run();
+			}
+		}
+	}
+	
+	public void terminate(){
+		if (!this.isAlive()) return;
+		this.toBeStopped = true;
+		this.interrupt();
 	}
 }
