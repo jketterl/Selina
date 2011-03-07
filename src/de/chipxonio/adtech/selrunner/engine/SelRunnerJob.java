d8f487fae88ce8e288777bf694a0cc9e384caa32
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-07 11:22:46 +0100

    create some api for saving & restoring (doesn't seem to work atm)

--- src/de/chipxonio/adtech/selrunner/engine/SelRunnerJob.java
+++ src/de/chipxonio/adtech/selrunner/engine/SelRunnerJob.java
@@ -1,5 +1,13 @@
 package de.chipxonio.adtech.selrunner.engine;
 
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Iterator;
 import java.util.Vector;
 
@@ -8,7 +16,6 @@ import de.chipxonio.adtech.selrunner.tests.TestResult;
 public class SelRunnerJob extends Vector<SelRunnerTask> implements SelRunnerTaskListener {
 	private static final long serialVersionUID = 6697337614166675395L;
 	private Vector<SelRunnerJobListener> listeners = new Vector<SelRunnerJobListener>();
-	//private LinkedList<SelRunnerTask> tasks = new LinkedList<SelRunnerTask>();
 	
 	public boolean add(SelRunnerTask task) {
 		task.addListener(this);
@@ -31,5 +38,41 @@ public class SelRunnerJob extends Vector<SelRunnerTask> implements SelRunnerTask
 	@Override
 	public void testingComplete(TestResult result) {
 		this.fireTestingComplete(result);
+	}
+	
+	public void saveToFile(File file) {
+		try {
+			XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(file)));
+			encoder.writeObject(this);
+			encoder.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+		oos.writeObject(this);
+		oos.close();
+		*/
+	}
+	
+	public static SelRunnerJob loadFromFile(File file) {
+		ObjectInputStream ois;
+		try {
+			ois = new ObjectInputStream(new FileInputStream(file));
+			SelRunnerJob job = (SelRunnerJob) ois.readObject();
+			ois.close();
+			return job;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
