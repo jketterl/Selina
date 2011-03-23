139f1d1d811dc56326e6358685868253a48667f5
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-23 11:33:50 +0100

    rewrite to ActiveVector

--- src/de/chipxonio/adtech/selrunner/packages/PackageList.java
+++ src/de/chipxonio/adtech/selrunner/packages/PackageList.java
@@ -4,21 +4,18 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Iterator;
 import java.util.Vector;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
-import javax.swing.ListModel;
-import javax.swing.event.ListDataEvent;
-import javax.swing.event.ListDataListener;
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
-public class PackageList extends Vector<Package> implements ListModel, TreeModel {
+import de.chipxonio.adtech.selrunner.util.ActiveVector;
+
+public class PackageList extends ActiveVector<Package> implements TreeModel {
 	private static final long serialVersionUID = -6282910026609075670L;
-	private Vector<ListDataListener> listeners = new Vector<ListDataListener>();
 	private Vector<TreeModelListener> treeListeners = new Vector<TreeModelListener>();
 	private Preferences preferences;
 	
@@ -40,33 +37,11 @@ public class PackageList extends Vector<Package> implements ListModel, TreeModel
 		} catch (BackingStoreException e) {
 		}
 	}
-	
-	@Override
-	public void addListDataListener(ListDataListener arg0) {
-		this.listeners.add(arg0);
-	}
-
-	@Override
-	public Object getElementAt(int arg0) {
-		return this.get(arg0);
-	}
-
-	@Override
-	public int getSize() {
-		return this.size();
-	}
-
-	@Override
-	public void removeListDataListener(ListDataListener arg0) {
-		this.listeners.remove(arg0);
-	}
 
 	@Override
 	public synchronized boolean add(Package e) {
 		boolean ret = super.add(e);
 		if (ret) {
-			int index = this.indexOf(e);
-			this.fireIntervalAdded(new ListDataEvent(this, ListDataEvent.INTERVAL_ADDED, index, index));
 			if (this.hasPreferences() && !e.hasPreferences()) {
 				String nodeName;
 				try {
@@ -92,8 +67,6 @@ public class PackageList extends Vector<Package> implements ListModel, TreeModel
 
 	@Override
 	public boolean remove(Object o) {
-		int index = this.indexOf(o);
-		this.fireIntervalRemoved(new ListDataEvent(this, ListDataEvent.INTERVAL_REMOVED, index, index));
 		Package p = (Package) o;
 		if (p.hasPreferences()) try {
 			p.getPreferences().removeNode();
@@ -101,21 +74,6 @@ public class PackageList extends Vector<Package> implements ListModel, TreeModel
 			e.printStackTrace();
 		}
 		return super.remove(o);
-	}
-
-	private void fireContentsChanged(ListDataEvent e) {
-		Iterator<ListDataListener> i = listeners.iterator();
-		while (i.hasNext()) i.next().contentsChanged(e);
-	}
-	
-	private void fireIntervalAdded(ListDataEvent e) {
-		Iterator<ListDataListener> i = listeners.iterator();
-		while (i.hasNext()) i.next().intervalAdded(e);
-	}
-	
-	private void fireIntervalRemoved(ListDataEvent e) {
-		Iterator<ListDataListener> i = listeners.iterator();
-		while (i.hasNext()) i.next().intervalRemoved(e);
 	}
 
 	@Override
