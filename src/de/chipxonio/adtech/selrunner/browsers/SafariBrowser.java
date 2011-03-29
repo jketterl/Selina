4f731e5e94b6f008a003851cb54f099f0b59fe72
Jakob Ketterl <jketterl@chipxonio.de>
2011-03-29 21:11:21 +0200

    this is how safari would work in theory

--- src/de/chipxonio/adtech/selrunner/browsers/SafariBrowser.java
+++ src/de/chipxonio/adtech/selrunner/browsers/SafariBrowser.java
@@ -1,16 +1,33 @@
 package de.chipxonio.adtech.selrunner.browsers;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.SeleneseCommandExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CommandExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import de.chipxonio.adtech.selrunner.hosts.Host;
 
 public class SafariBrowser extends Browser {
 	@Override
 	public WebDriver getDriver(Host host) {
-		// TODO Auto-generated method stub
+		try {
+			URL seleniumServer = new URL("http://localhost:4444");
+			URL remoteUrl = new URL("http://www.google.com/");
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setBrowserName("safari");
+			CommandExecutor executor = new SeleneseCommandExecutor(seleniumServer, remoteUrl, capabilities);
+			return new RemoteWebDriver(executor, capabilities);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
-
+	
 	@Override
 	public String getName() {
 		return "Safari";
