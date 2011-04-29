9951697d6a3fa780059fb07956e8fda7c1f22748
Jakob Ketterl <jketterl@chipxonio.de>
2011-04-29 15:16:42 +0200

    use a more verbose message

--- src/de/chipxonio/adtech/selina/tests/outcomes/ExceptionFailure.java
+++ src/de/chipxonio/adtech/selina/tests/outcomes/ExceptionFailure.java
@@ -6,6 +6,6 @@ import de.chipxonio.adtech.selina.tests.TestCase;
 
 public class ExceptionFailure extends Failure {
 	public ExceptionFailure(Class<? extends TestCase> c, Method m, Exception e) {
-		super(c, m, e.getMessage());
+		super(c, m, e.getClass().getName() + " with message \"" + e.getMessage() + "\"");
 	}
 }
