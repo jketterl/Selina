d207580aab158818d29fbd6634eed06fbdeb69f4
Jakob Ketterl <jketterl@chipxonio.de>
2011-05-04 09:29:36 +0200

    that's how it's supposed to look

--- src/de/chipxonio/adtech/selina/gui/components/FailureDetailPanel.java
+++ src/de/chipxonio/adtech/selina/gui/components/FailureDetailPanel.java
@@ -2,6 +2,7 @@ package de.chipxonio.adtech.selina.gui.components;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.net.URL;
 
 import javax.swing.BorderFactory;
@@ -39,38 +40,45 @@ public class FailureDetailPanel extends JPanel {
 	 * @return void
 	 */
 	private void initialize() {
-		this.setLayout(new GridBagLayout());
 		this.setBorder(BorderFactory.createTitledBorder(null, "Failure Details", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
+		this.setLayout(new GridBagLayout());
 
-		GridBagConstraints gc1 = new GridBagConstraints();
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.weightx = 0; gc.weighty = 0;
+		
+		// add labels
+		gc.insets = new Insets(2, 5, 2, 5);
+		
 		jLabel = new JLabel();
 		jLabel.setText("Browser URL:");
-		gc1.gridx = 0; gc1.gridy = 0;
-		gc1.anchor = GridBagConstraints.WEST;
-		this.add(jLabel, gc1);
-
-		GridBagConstraints gc2 = new GridBagConstraints();
-		gc2.gridx = 1; gc2.gridy = 0;
-		gc2.fill = GridBagConstraints.HORIZONTAL;
-		this.add(getUrlTextField(), gc2);
+		gc.gridx = 0; gc.gridy = 0;
+		gc.anchor = GridBagConstraints.WEST;
+		this.add(jLabel, gc);
 
 		jLabel1 = new JLabel();
 		jLabel1.setText("Message:");
-		GridBagConstraints gc3 = new GridBagConstraints();
-		gc3.gridx = 0; gc3.gridy = 1;
-		gc3.anchor = GridBagConstraints.WEST;
-		this.add(jLabel1, gc3);
+		gc.gridx = 0; gc.gridy = 1;
+		gc.anchor = GridBagConstraints.WEST;
+		this.add(jLabel1, gc);
 		
-		GridBagConstraints gc4 = new GridBagConstraints();
-		gc4.gridx = 1; gc4.gridy = 1;
-		gc4.fill = GridBagConstraints.HORIZONTAL;
-		this.add(getMessageTextField(), gc4);
+		// add text fields
+		gc.weightx = 1;
 		
-		GridBagConstraints gc5 = new GridBagConstraints();
-		gc5.gridx = 0; gc5.gridy = 2;
-		gc5.gridwidth = GridBagConstraints.REMAINDER;
-		gc5.fill = GridBagConstraints.BOTH;
-		this.add(getJScrollPane(), gc5);
+		gc.gridx = 1; gc.gridy = 0;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		this.add(getUrlTextField(), gc);
+
+		gc.gridx = 1; gc.gridy = 1;
+		gc.fill = GridBagConstraints.HORIZONTAL;
+		this.add(getMessageTextField(), gc);
+		
+		// add list box
+		gc.weighty = 1;
+
+		gc.gridx = 0; gc.gridy = 2;
+		gc.gridwidth = GridBagConstraints.REMAINDER;
+		gc.fill = GridBagConstraints.BOTH;
+		this.add(getJScrollPane(), gc);
 	}
 
 	/**
@@ -132,3 +140,5 @@ public class FailureDetailPanel extends JPanel {
 		return stackTraceList;
 	}
 }
+
+
